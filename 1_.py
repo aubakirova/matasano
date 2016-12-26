8f70060de8605134e4ea1afa555d14fa2af22de2
Sun77789 <aubakirova.m.m@gmail.com>
2016-12-26 14:53:29 +0600

    Asserting

--- 1_.py
+++ 1_.py
@@ -2,10 +2,9 @@ def convert_hex(input):
     return input.decode("hex").encode("base64")
 
 def test_convert_hex():
-    if convert_hex('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d') == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n':
-        print('Success')
-    else:
-        print('Fail in test_convert_hex')
+    x = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n'
+    assert_(convert_hex(x), result)
 
 def xor(x, y):
     x_decode = x.decode('hex')
@@ -18,10 +17,14 @@ def xor(x, y):
 def test_xor():
     x = '1c0111001f010100061a024b53535009181c'
     y = '686974207468652062756c6c277320657965'
-    if xor(x, y) == '746865206b696420646f6e277420706c6179':
+    e = '746865206b696420646f6e277420706c6179'
+    assert_(xor(x,y), e)
+
+def assert_(actual, expected):
+    if actual == expected:
         print('Success')
     else:
-        print('Failure %s' % xor(x, y))
+        print('Failure: expected %s, but got %s' % (actual, expected))
 
 def tests():
     test_convert_hex()
