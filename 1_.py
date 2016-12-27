5fb9b11019aa3cd35cc1d006b8cde0883b07996c
Sun77789 <aubakirova.m.m@gmail.com>
2016-12-27 19:08:52 +0600

    Now that the party is jumping

--- 1_.py
+++ 1_.py
@@ -1,3 +1,6 @@
+import array
+import re
+
 def convert_hex(input):
     return input.decode("hex").encode("base64")
 
@@ -22,23 +25,51 @@ def test_xor():
 
 def single_byte_xor(encoded):
     # Find the message, given that the key is one character long
-    keys = [str(b) * (len(encoded)-1) for b in range(0, 255)]
+    byte_array = array.array('B', encoded.decode('hex'))
     best = (0.0, '', '')
-    for key in keys:
+    for key in xrange(0,256):
         try:
-            tmp = xor(encoded, key).decode('hex')
-            score = sum([1 if ch.isalpha() else 0 for ch in tmp])
+            tmp = [key ^ b for b in byte_array]
+            result_array = "".join("{:c}".format(b) for b in tmp)
+            if not re.match("^[\w '-?!.,:;()&]+$", result_array):
+                continue
+            score = sum([1 if ch.isalpha() else 0 for ch in result_array])
             if score > best[0]:
-                best = (score, tmp, key)
+                best = (score, result_array, key)
         except:
             pass
     # as it turns out, best is "Cooking MCs like a pound of bacon"
-    return best[1]
+    return best
+
+def read_file(name):
+    f = open('files/' + name, 'r')
+    return f
+
+def detect_single_line():
+    filename = '4.txt'
+    f = read_file(filename)
+    # let's keep 3 best sentences
+    best = [(0.0, '', '')] * 3
+    def sort_score(t):
+        return -t[0]
+    for line in f:
+        if line[-1] == '\n':
+            line = line[:-1]
+        current = single_byte_xor(line)
+        tmp = sorted(best + [current], key=sort_score)
+        best = tmp[:-1]
+    # print best
+    return best[0]
+
+def test_detect_single_line():
+    actual = detect_single_line()
+    expected = 'Now that the party is jumping\n'
+    assert_(actual[1], expected)
 
 def test_single_byte_xor():
     actual = single_byte_xor('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
     expected = "Cooking MC's like a pound of bacon"
-    assert_(actual, expected)
+    assert_(actual[1], expected)
 
 def assert_(actual, expected):
     if actual == expected:
@@ -50,7 +81,9 @@ def tests():
     test_convert_hex()
     test_xor()
     test_single_byte_xor()
+    test_detect_single_line()
 
 if __name__ == "__main__":
     tests()
+    detect_single_line()
 
