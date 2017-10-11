76b6d2c62d2de38695b3169708efe0b8d7d3121b
Malika Aubakirova <aubakirova.m.m@gmail.com>
2017-10-10 21:44:42 -0500

    trial

--- ex1.py
+++ ex1.py
@@ -23,6 +23,7 @@ def test_xor():
     x = '1c0111001f010100061a024b53535009181c'
     y = '686974207468652062756c6c277320657965'
     e = '746865206b696420646f6e277420706c6179'
+    # trial comment
     assert_(xor(x,y), e)
 
 def single_byte_xor_helper(byte_array):
